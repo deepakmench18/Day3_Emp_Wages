a97eef8064d9f488e111315d703898360e3debf0
deepakmench18 <deepakmench18@gmail.com>
2021-06-21 20:31:14 +0530

    Calculated Daily wages

--- Emp_wages.java
+++ Emp_wages.java
@@ -1,14 +1,25 @@
-
 public class Emp_wages {
 
 	public static void main(String[] args) {
 		int Full_time = 1;
+		int WagePerHr = 20;
+		int FullDayHr = 8;
+		int isPresent = 1, isAbsent = 0;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == Full_time)
 			System.out.println("Employee is present");
 		else
 			System.out.println("Employee is Abscent");
-
+        int Daily_wage = WagePerHr * FullDayHr;
+        if (empCheck == isPresent)
+        {
+        	System.out.println("Employee Daily wager = " + Daily_wage);
+        }
+        else
+        {
+        	System.out.println("Employee Daily Wager" + isAbsent);
+        }
 	}
 
 }
+
