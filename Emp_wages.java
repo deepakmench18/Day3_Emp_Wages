d9696d6611141614b85061f6c9ce7735b129f014
deepakmench18 <deepakmench18@gmail.com>
2021-06-21 20:45:22 +0530

    ADD Part time Employee

--- Emp_wages.java
+++ Emp_wages.java
@@ -1,10 +1,12 @@
+
 public class Emp_wages {
 
 	public static void main(String[] args) {
 		int Full_time = 1;
 		int WagePerHr = 20;
 		int FullDayHr = 8;
-		int isPresent = 1, isAbsent = 0;
+		int ParttimeHr = 4;
+		int isPresent = 1, isAbsent = 0, isParttime = 2;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == Full_time)
 			System.out.println("Employee is present");
@@ -18,6 +20,19 @@ public class Emp_wages {
         else
         {
         	System.out.println("Employee Daily Wager" + isAbsent);
+        }
+        int Parttimewage = WagePerHr * ParttimeHr;
+        if (empCheck == isPresent)
+        {
+        	System.out.println("Employee Full Day Wage " + Daily_wage);
+        }
+        else if(empCheck == isParttime)
+        {
+        	System.out.println("Employee Part time wage" + Parttimewage);
+        }
+        else
+        {
+        	System.out.println("Employee Daily Wages " + isAbsent);
         }
 	}
 
